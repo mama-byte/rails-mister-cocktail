0fe640f1888df5fce154d7f160c40073b0ccfc40
Ruthie Haskell <ruthie.haskell@gmail.com>
2019-12-18 16:46:24 +0100

    front end

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Cleaning database...'
-Ingedient.destroy_all
+Ingredient.destroy_all
 
 puts 'Creating ingredients...'
-ingredients_attributes = ([{ name: 'lemon' }, { name: 'ice' }, { name: 'mint_leaves' }])
-
-Ingedient.create(ingredients_attributes)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
 puts 'Finished!'
