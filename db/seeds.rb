188de96e8a3cf6b3f86b762c9fa8a6a270902894
Ruthie Haskell <ruthie.haskell@gmail.com>
2019-12-19 15:31:45 +0100

    adding seed to db, changing button colors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
+require 'json'
+require 'open-uri'
+
+puts "Destroy ingredients"
+Ingredient.destroy_all if Rails.env.development?
+
+puts "Destroy Cocktails"
+Cocktail.destroy_all if Rails.env.development?
+
+puts "Create ingredients"
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+ingredients["drinks"].each do |ingredient|
+  i = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "create #{i.name}"
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,11 +22,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts 'Cleaning database...'
-Ingredient.destroy_all
+# puts 'Cleaning database...'
+# Ingredient.destroy_all
 
-puts 'Creating ingredients...'
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-puts 'Finished!'
+# puts 'Creating ingredients...'
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "soda water")
+# Ingredient.create(name: "tonic")
+# Ingredient.create(name: "lime")
+# Ingredient.create(name: "sugar")
+# Ingredient.create(name: "rum")
+# Ingredient.create(name: "")
+# Ingredient.create(name: "")
+# Ingredient.create(name: "")
+# Ingredient.create(name: "")
+# Ingredient.create(name: "")
+# Ingredient.create(name: "")
+# Ingredient.create(name: "")
+# puts 'Finished!'
